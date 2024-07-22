740b879a2ca505ebd3e9d612646816bdaee84f4a
Jagdish Umashankar Vishwakarma <112471213+Jagdish-Vishwakarma@users.noreply.github.com>
2024-07-21 20:29:59 -0500

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os
+import streamlit as st
+from PyPDF2 import PdfReader
+from dotenv import load_dotenv
 
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
 
 
 import os
